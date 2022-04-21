4a94ca4c3de68c0fd15b5f25c9ec2938baa41fc2
Michał Murawski <mmurawski777@gmail.com>
2022-04-21 17:14:46 +0200

    Initial commit

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_dependencies():
 
 setup(
     name='osdu_client',
-    version='0.1.3',
+    version='0.1.4',
     author="Michal Murawski",
     author_email="mmurawski777@gmail.com",
     description="OSDU API Client",
---
+++ src/osdu_client/jsonschema.py
@@ -0,0 +1,24 @@
+from jsonschema import RefResolver, Draft7Validator, RefResolutionError
+from .services.schema_api import SchemaAPIClient, SchemaAPIError
+
+
+class OSDURefResolver(RefResolver):
+    def __init__(self, *args, schema_client: SchemaAPIClient, **kwargs) -> None:
+        self.schema_client = schema_client
+        super().__init__(*args, **kwargs)
+
+    def resolve_remote(self, uri):
+        if uri.startswith('/SOMETHING'):
+            try:
+                return self.schema_client.get_schema(id=uri)
+            except SchemaAPIError as e:
+                raise RefResolutionError(str(e)) from e
+        else:
+            # fallback to original
+            return super(RefResolver, self).resolve_remote(uri)
+
+
+def validate(record, schema, schema_client):
+    resolver = OSDURefResolver(None, referrer=None, store={}, schema_client=schema_client)
+    validator = Draft7Validator(schema, resolver=resolver)
+    return validator.iter_errors(record)
